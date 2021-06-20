2928698f0356fc14dcada7ee0f66a91a5f564fb9
DimaOanaTeodora <61749814+DimaOanaTeodora@users.noreply.github.com>
2021-06-20 14:50:06 +0300

    Update FLP3mWriter.hs

--- FLP3mWriter.hs
+++ FLP3mWriter.hs
@@ -1,6 +1,6 @@
-
---- Monada Writer Writer log a (log - efectul ca unul de logare)
-
+--- Monada Writer
+-- Writer este constructorul 
+-- cu runWriter se face afisarea
 newtype WriterS a = Writer { runWriter :: (a, String) } 
 
 
@@ -21,56 +21,30 @@ instance  Applicative WriterS where
 instance  Functor WriterS where              
   fmap f ma = pure f <*> ma     
 
-tell :: String -> WriterS () 
+tell :: String -> WriterS () -- functie auxiliara
 tell log = Writer ((), log)
   
 logIncrement :: Int  -> WriterS Int
-logIncrement x = Writer(x+1, "Am incrementat " ++ (show x) ++ " ")
--- runWriter (logIncrement 3) => (4,"Am incrementat 3")
--- runWriter $ (logIncrement 3 >> logIncrement 4) => (5,"Am incrementat 3 Am incrementat 4 ")
--- runWriter $ (logIncrement 3 >>= (\x -> logIncrement (2*x))) => (9,"Am incrementat 3 Am incrementat 8 ")
--- Varianta 2
--- do
--- tell ("Am incrementat "  ++ show x)
--- return (x+1)
+logIncrement x = Writer (x+1, "Am incrementat " ++ show(x) ++ " ")
+-- runWriter (logIncrement 3) => (4,"Am incrementat 3 ")
+
+-- Varianta DO ---
+logIDo :: Int  -> WriterS Int
+logIDo x = do
+      tell("Am incrementat " ++ show(x) ++ " ")
+      return (x+1)
+
+-- varianta DO1
 logIncrementN :: Int -> Int -> WriterS Int
-logIncrementN x n =  do
-                      tell ("Incrementam: " ++ show x)
-                      if n > 1 then logIncrementN (x+1) (n-1)
-                      else return (x+1)
--- Varianta 2 ma folosesc de logIncrement x
--- logIncrementN x 1 = logIncrement x
---  logIncrement x n = do 
---                        y <- logIncrementN x (n-1)
---                        logIncrement y
-isPos :: Int -> WriterS Bool
-isPos x = if (x>= 0) then (Writer (True, "poz")) else (Writer (False, "neg"))
+logIncrementN x 1 = logIncrement x
+logIncrementN x n = do 
+                  y <- logIncrementN x (n-1)
+                  logIncrement y
+-- runWriter $ logIncrementN 2 4
 
-mapWriterS :: (a -> WriterS b) -> [a] -> WriterS [b]
-mapWriterS f [] = return []
-mapWriterS f (x:xs) = do
-                        y <- f x -- f x e valoarea monadica deci trebuie sa extrag din el folsind '<-'
-                        ys <- mapWriterS f xs
-                        return (y:ys)
-{- Vezi cod pt WriterI
-              modificare monada
-newtype WriterI a = Writer { runWriter :: (a, Int) } --(Int, +, 0) monoid
-
-instance  Monad WriterI where
-  return va = Writer (va, 0) -- se schimba aici
-  ma >>= k = let (va, log1) = runWriter ma
-                 (vb, log2) = runWriter (k va)
-             in  Writer (vb, log1 + log2) -- se schimba aici
-Raman neschimbate Aplicative si Functor si tell
-Redefinire functii de logl si loglN
-logl :: String -> WriterI String
-logl s = do
-          tell (length s)
-          return (s ++ "!")
-loglN :: String -> Int -> WriterI String
-loglN x l = logl x
-;pglN x n = do
-            y <- logl x
-            loglN y (n-1)
-
--}
+-- varianta DO2
+logINDo:: Int -> Int -> WriterS Int
+logINDo x n =  do
+      tell ("Am incrementat " ++ show x ++",")
+      if n > 1 then logINDo (x+1) (n-1)
+      else return (x+1)
