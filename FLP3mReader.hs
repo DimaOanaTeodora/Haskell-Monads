8195311c32be335ff19c5853a167486a13e7158e
DimaOanaTeodora <61749814+DimaOanaTeodora@users.noreply.github.com>
2021-06-20 18:28:53 +0300

    Update FLP3mReader.hs

--- FLP3mReader.hs
+++ FLP3mReader.hs
@@ -1,19 +1,11 @@
-
 ---Monada Reader
-
-
 newtype Reader env a = Reader { runReader :: env -> a }
--- runReader :: Reader env a -> (env -> a)
--- aici runReader :: Reader Person String -> (Person -> String) 
--- runReader mp :: Person -> String 
 
 instance Monad (Reader env) where
   return x = Reader (\_ -> x)
   ma >>= k = Reader f
     where f env = let a = runReader ma env
                   in  runReader (k a) env
-
-
 
 instance Applicative (Reader env) where
   pure = return
@@ -26,49 +18,36 @@ instance Functor (Reader env) where
   fmap f ma = pure f <*> ma
 
 
-ask :: Reader env env
+ask :: Reader env env -- functie auxiliara
 ask = Reader id
 
-local :: (env -> env) -> Reader env a -> Reader env a
-local f ma = Reader $ (\env -> (runReader ma)(f env))
-
--- Reader Person String
-
+--- fara monade -----
 data Person = Person { name :: String, age :: Int }
 
 showPersonN :: Person -> String
-showPersonN (Person name age )= "NAME: " ++ name
+showPersonN (Person name age) = "Nume: " ++ name
 
 showPersonA :: Person -> String
-showPersonA (Person name age ) = "AGE: " ++ show (age)
-
+showPersonA (Person name age) = "Varsta: " ++ show age
 
 showPerson :: Person -> String
-showPerson p = "(" ++ showPersonN p ++ "," ++ showPersonA p ++ ")"
--- showPerson (Person "A" 10) => "(NAME: A,AGE: 10)"
+showPerson person =  (showPersonN person) ++ ", " ++ (showPersonA person)
+-- showPerson (Person "Ionel" 12) SAU showPerson $ Person "ada" 20   
 
-
--- Asta e o functie deci trebuie sa fac runReader mshowPersonN $
-mshowPersonN ::  Reader Person String 
+---- cu monada Reader -----
+mshowPersonN ::  Reader Person String
 mshowPersonN = do
-               p <- ask           
-               return ("NAME: " ++ (name p))
--- (runReader mshowPersonN) $ (Person "A" 10) => "NAME: A"
+      p <- ask --  extrag Person din Reader Person
+      return ("Nume: " ++ name p)
 
-{- varianta fara scriere monadica
-mshowPersonN ::  Reader Person String --Reader f unde f :: Person -> String
-mshowPersonN = Reader f               --runReader mshowPersonN -> f
-                where 
-                  f (Person n a) = "NAME: " ++ n
--}
 mshowPersonA ::  Reader Person String
-mshowPersonA = Reader f
-                where 
-                  f (Person n a) =  "AGE: " ++ show (a)
+mshowPersonA = do
+      p <- ask --  extrag Person din Reader Person
+      return ("Varsta: " ++ show (age p)) -- le bag in monada iar prin return
 
 mshowPerson :: Reader Person String
 mshowPerson = do
-                n <- mshowPersonN 
-                a <- mshowPersonA 
-                return ("(" ++ n ++ "," ++ a ++ ")")
---(runReader mshowPerson) $ (Person "A" 10) => "(NAME: A,AGE: 10)"
+      nume <- mshowPersonN -- trebuie scoase din monadele din cauza ca le-am intors in return
+      varsta <- mshowPersonA
+      return (nume ++ "," ++ varsta)
+-- runReader mshowPerson $ Person "ada" 20
