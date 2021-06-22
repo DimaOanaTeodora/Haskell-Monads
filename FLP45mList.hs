1b88f9c99ab2aa1eab5f3cf135d147126cb08022
DimaOanaTeodora <61749814+DimaOanaTeodora@users.noreply.github.com>
2021-06-22 15:48:40 +0300

    Update FLP45mList.hs

--- FLP45mList.hs
+++ FLP45mList.hs
@@ -1,6 +1,9 @@
---- Monada Lista
+--- Monada List -- definita by default
+-- Dispare showM pentru ca e deja definit si el
 
 --- Limbajul si  Interpretorul
+
+type M a = [a] --NOU
 
 type Name = String
 
@@ -9,57 +12,50 @@ data Term = Var Name
           | Term :+: Term
           | Lam Name Term
           | App Term Term
-          | Fail 
-          | Ned [Term] -- expresie formata din oricate valori vrem
-          | Amb Term Term
+          | Fail            -- NOU
+          | Amb Term Term   -- NOU
   deriving (Show)
 
 data Value = Num Integer
-           | Fun (Value -> M Value)
+           | Fun (Value -> M Value) -- Value -> Identity Value
            | Wrong
 
-instance Show Value where
+instance Show Value where 
  show (Num x) = show x
  show (Fun _) = "<function>"
- show Wrong   = "<wrong>"
+ show Wrong = "<wrong>"
 
-type Environment = [(Name, Value)] -- [(String, Value)]
+type Environment = [(Name, Value)] --[(String, Value)]
 
-type M a = [a]-- nu mai trebuie scrisa pt ca e deja monada
-
-{-- nu mai am nevoie de el
-showM :: Show a => M a -> String
-showM (Just value) = show value
-showM Nothing = error "Error"
--}
-
-interp :: Term -> Environment -> M Value -- Term -> [(String, Value)] -> Either Value
-interp Fail _ = []
+interp :: Term -> [(Name, Value)] -> M Value
+-- Term -> [(String, Value)] ->  Either Value
+interp Fail env = [] -- NOU
+interp (Amb t1 t2) env = (interp t1 env) ++ (interp t2 env) -- NOU
+interp (Var string) env = aux (lookup string env )
+  where
+    aux (Just value) = return value
+    aux Nothing = return Wrong
 interp (Con n) env = return (Num n)
-interp (Var string) env =  aux (lookup string env )
--- (lookup string env) => Maybe Value : Num Integer, Fun, Wrong..
-      where
-            aux (Just value) = return value
-            aux Nothing = return Wrong
-interp (Amb t1 t2) env = (interp t1 env) ++ (interp t2 env) 
-interp (Ned xs) env = concat [interp t env| t<- xs]
 interp (t1 :+: t2) env = do
-       x <- interp t1 env
-       y <- interp t2 env
-       add x y
-       where 
-             add (Num x) (Num y) = return (Num (x+y))
-             add _ _ = return Wrong
-
+  x <- interp t1 env -- x de tipul Value
+  y <- interp t2 env -- y de tipul Value
+  auxsum x y
+  where
+    auxsum (Num n1)(Num n2) = return (Num (n1+n2))
+    auxsum _ _ = return Wrong
 interp (Lam string t) env = return $ Fun $ \v -> interp t ((string,v):env)
 interp (App t1 t2) env = do
-       f <- interp t1 env
-       x <- interp t2 env
-       app f x 
-       where 
-             app (Fun func) x = func x
-             app _ _ = return Wrong
+  x <- interp t1 env
+  y <- interp t2 env
+  auxapp x y 
+  where
+      auxapp (Fun f) value = f value -- Fun (Value -> M Value)
+      auxapp _ _ = return Wrong
 
--- !! NU mai am nevoie de showM !!
-term0 = (App (Lam "x" (Var "x" :+: Var "x")) (Amb (Con 1) (Con 2))) 
--- interp term0 [] => [2,4]
+test :: Term -> String
+test t = show $ interp t [] -- show-ul by default pt Maybe ca sa transforme in string
+
+term0:: Term
+term0 = (App (Lam "x" (Var "x" :+: Var "x")) (Amb (Con 1) (Con 2)))
+-- test term0 => "[2,4]"
+-- interp t [] => [2,4]
