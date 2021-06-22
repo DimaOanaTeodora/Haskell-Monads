148c547f3e9f4ba808a874fc2a45e222a2abf9db
DimaOanaTeodora <61749814+DimaOanaTeodora@users.noreply.github.com>
2021-06-22 15:35:57 +0300

    Update FLP45mEitherString.hs

--- FLP45mEitherString.hs
+++ FLP45mEitherString.hs
@@ -1,6 +1,10 @@
---- Monada Either String (Right x | Left y)
+--- Monada Either -- definita by default
+-- Dispare showM pentru ca e deja definit si el
+-- Trebuie sa specific Left cand returnez o eroare
 
 --- Limbajul si  Interpretorul
+
+type M = Either String
 
 type Name = String
 
@@ -12,51 +16,46 @@ data Term = Var Name
   deriving (Show)
 
 data Value = Num Integer
-           | Fun (Value -> M Value)
+           | Fun (Value -> M Value) -- Value -> Identity Value
            | Wrong
 
-instance Show Value where
+instance Show Value where 
  show (Num x) = show x
  show (Fun _) = "<function>"
- show Wrong   = "<wrong>"
+ show Wrong = "<wrong>"
 
-type Environment = [(Name, Value)] -- [(String, Value)]
+type Environment = [(Name, Value)] --[(String, Value)]
 
-type M = Either String -- nu mai trebuie scrisa pt ca e deja monada
-
-{-- nu mai am nevoie de el
-showM :: Show a => M a -> String
-showM (Just value) = show value
-showM Nothing = error "Error"
--}
-
-interp :: Term -> Environment -> M Value -- Term -> [(String, Value)] -> Either Value
+interp :: Term -> [(Name, Value)] -> M Value
+-- Term -> [(String, Value)] ->  Either Value
+interp (Var string) env = aux (lookup string env )
+  where
+    aux (Just value) = return value
+    aux Nothing = (Left (show Wrong)) -- sau : (Left "eroare lookup")
 interp (Con n) env = return (Num n)
-interp (Var string) env =  aux (lookup string env )
--- (lookup string env) => Maybe Value : Num Integer, Fun, Wrong..
-      where
-            aux (Just value) = return value
-            aux Nothing = (Left "Variable not found")
 interp (t1 :+: t2) env = do
-       x <- interp t1 env
-       y <- interp t2 env
-       add x y
-       where 
-             add (Num x) (Num y) = return (Num (x+y))
-             add _ _ = (Left "Not a number")
-
+  x <- interp t1 env -- x de tipul Value
+  y <- interp t2 env -- y de tipul Value
+  auxsum x y
+  where
+    auxsum (Num n1)(Num n2) = Right (Num (n1+n2))
+    auxsum _ _ = (Left (show Wrong))
 interp (Lam string t) env = return $ Fun $ \v -> interp t ((string,v):env)
 interp (App t1 t2) env = do
-       f <- interp t1 env
-       x <- interp t2 env
-       app f x 
-       where 
-             app (Fun func) x = func x
-             app _ _ = (Left "Not a function")
+  x <- interp t1 env
+  y <- interp t2 env
+  auxapp x y 
+  where
+      auxapp (Fun f) value = f value -- Fun (Value -> M Value)
+      auxapp _ _ = (Left (show Wrong))
 
--- !! NU mai am nevoie de showM !!
-term0 = (App (Lam "x" (Var "x" :+: Var "x")) (Con 10 :+: Con 11))
--- interp term0 [] => "Just 42"
+test :: Term -> String
+test t = show $ interp t [] -- show-ul by default pt Maybe ca sa transforme in string
 
-pgm = App (Con 10) (Con 11)
--- interp pgm [] => Left "Not a function"
+term0:: Term
+term0 = App (Lam "x" ((Var "x") :+: (Var "x"))) ((Con 10) :+:  (Con 11))
+-- test term0 => "Right 42"
+-- interp t [] => Right 42
+
+term1 = App (Con 10) (Con 11)
+-- test term1 => "Left \"<wrong>\""
